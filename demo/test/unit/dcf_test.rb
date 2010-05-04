c9701be92c9e2a03a21d4b6675a3ef66d9380a4f
Wataru Ito <wito@sakura-computer.co.jp>
2010-05-04 16:10:57 +0900

    test

--- demo/test/unit/dcf_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class DcfTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- demo/test/unit/helpers/drves_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DrvesHelperTest < ActionView::TestCase
-end
